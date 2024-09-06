fa153d0f5fd3be19837ecccbeda0a56ef3aeceb9
Tristan Galang <tristangalang12@gmail.com>
2024-09-06 16:25:34 -0700

    switch from 24 hour base to 12 hour base for the clock component

--- script.js
+++ script.js
@@ -5,11 +5,15 @@ const minuteEl = document.querySelector('.minute');
 // Setting up the time
 const updateTime = () => {
     const currentTime = new Date();
-    const currentHour = currentTime.getHours();
+    let currentHour = currentTime.getHours();
     const currentMinute = currentTime.getMinutes();
 
+    if (currentHour > 12) { // gets the 12 hour base instead of 24 hour base
+        currentHour -= 12;
+    }
+
     hourEl.textContent = currentHour.toString();
-    minuteEl.textContent = currentMinute.toString();
+    minuteEl.textContent = currentMinute.toString().padStart(2, '0');
 }
 setInterval(updateTime, 1000);
 updateTime();
