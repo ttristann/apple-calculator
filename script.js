1c476864d15b4695e40de564c4792d7d91cc62c2
Tristan Galang <tristangalang12@gmail.com>
2024-09-06 16:47:00 -0700

    started working on the js script

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
 // DOM Elements
+// top container
 const hourEl = document.querySelector('.hour');
 const minuteEl = document.querySelector('.minute');
+// screen
+const displayEl = document.querySelector('.display');
+// functions
+const acEl = document.querySelector('.ac');
+const pmEl = document.querySelector('.pm');
+const percentEl = document.querySelector('.percent');
+//operators + decimal
+const additionEl = document.querySelector('.addition');
+const subtractionEl = document.querySelector('subtraction');
+const divisionEl = document.querySelector('.division');
+const multiplicationEl = document.querySelector('.multiplication');
+const equalEl = document.querySelector('.equal');
+const decimalEl = document.querySelector('.decimal');
+// numbers
+const number0El = document.querySelector('.number-0');
+const number1El = document.querySelector('.number-1');
+const number2El = document.querySelector('.number-2');
+const number3El = document.querySelector('.number-3');
+const number4El = document.querySelector('.number-4');
+const number5El = document.querySelector('.number-5');
+const number6El = document.querySelector('.number-6');
+const number7El = document.querySelector('.number-7');
+const number8El = document.querySelector('.number-8');
+const number9El = document.querySelector('.number-9');
+// container to hold all of the nunmber button elements
+const numberElArray = [
+    number0El, number1El, number2El, number3El, number4El,
+    number5El, number6El, number7El, number8El, number9El
+];
+
+// Functions
+
+// Event Listeners to numbers and buttons
+for (let i=0; i < numberElArray.length; ++i) {
+    const numberEl = numberElArray[i];
+    numberEl.addEventListener('.click', () => {
+        // complete functions
+    });
+}
 
 // Setting up the time
 const updateTime = () => {
